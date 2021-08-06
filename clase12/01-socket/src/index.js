81c91daebac78fa6433e62e5e08259cfefce8f6d
Cristian <ccinetto@edrans.com>
2021-08-06 17:35:55 -0300

    clase12

---
+++ clase12/01-socket/src/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+import * as http from 'http';
+import io from 'socket.io';
+
+/** INICIALIZACION API con EXPRESS */
+const app = express();
+const puerto = 8080;
+
+const publicPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicPath));
+
+const myServer = http.Server(app);
+
+myServer.listen(puerto, () => console.log('Server up en puerto', puerto));
+
+app.get('/hola', (req, res) => {
+  res.json({ msg: 'hola' });
+});
+
+const myWSServer = io(myServer);
+
+const messages = [];
+
+myWSServer.on('connection', function (socket) {
+  console.log('Un cliente se ha conectado');
+
+  socket.on('new-message', function (data) {
+    messages.push(data);
+    socket.emit('messages', messages);
+  });
+});
