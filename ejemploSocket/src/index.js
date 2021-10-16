08d5b29eef9e079725ec5f1fe055fc532005f169
Cristian Cinetto <cristiancinetto@gmail.com>
2021-10-16 15:26:01 -0300

    clase 31

--- ejemploSocket/src/index.js
+++ ejemploSocket/src/index.js
@@ -1,4 +1,27 @@
-import server from './services/server';
-const puerto = 8080;
+import fs from 'fs/promises';
 
-server.listen(puerto, console.log('SERVER UP!!', puerto));
+const filePath = './miarchivo.txt';
+
+fs.readdir(__dirname)
+  .then((data) => {
+    const miData = data.toString();
+    console.log(`DATA File Actual => ${miData}`);
+    return fs.appendFile('texto.txt', 'HOLA\n', 'utf8');
+  })
+  .then((resp) => {
+    console.log(resp);
+    return fs.readFile('texto.txt');
+  })
+  .then((data) => {
+    const miData = data.toString();
+    console.log(`DATA File Nueva => ${miData}`);
+  })
+  .catch((err) => {
+    console.log('Ataje el error');
+    console.log(err);
+  })
+  .finally(() => {
+    console.log('ESTO SE EJECUTA SI O SI');
+  });
+
+console.log('ULTIMA LINEA DE CODIGO');
